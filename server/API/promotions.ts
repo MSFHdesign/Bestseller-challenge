301723524c505996f2fa87f773e44634cc37402a
Michael Hovgaard <90240516+MSFHdesign@users.noreply.github.com>
2025-03-02 21:23:52 +0100

    Delete server/API directory

--- server/API/promotions.ts
+++
@@ -1,8 +0,0 @@
-import { defineEventHandler } from 'h3';
-import data from '../data/data.json';
-
-export default defineEventHandler(async (event) => {
-  return {
-    promotionalSpots: data.promotionalSpots || []
-  };
-});
