9835c4c98e7704dbac178c000d40c383b3a19c97
Michael Hovgaard <hovgaard1990@gmail.com>
2025-03-02 21:22:46 +0100

    Ensure API folder is tracked

--- server/api/categories.ts
+++
@@ -1,6 +0,0 @@
-import { defineEventHandler } from 'h3';
-import data from '../data/data.json';
-
-export default defineEventHandler(() => {
-  return { categories: Array.isArray(data.categories) ? data.categories : [] };
-}); 
